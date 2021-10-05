d99688736d54533c923de0c0818675af318b1d59
fortunehrjr <83907042+fortunehrjr@users.noreply.github.com>
2021-10-05 20:12:12 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const faders = document.querySelectorAll('.fade-in')
+
+const options = {
+    threshold: 1
+
+}
+
+const appearOnScroll = new IntersectionObserver (function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('appear')
+            appearOnScroll.unobserve(entry.target)
+        }
+    })
+}, options)
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader)
+})
+
+console.log('agh')
